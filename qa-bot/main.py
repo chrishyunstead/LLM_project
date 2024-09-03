ca1e7c4fce6bc3601d3334fd1edb8665c7349f05
Hyunsoo Jeong <hyeonsuj48@gmail.com>
2024-09-03 22:45:11 +0900

    initial commit

---
+++ qa-bot/main.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+prompt = ChatPromptTemplate.from_messages(
+    [ ("system", "당신은 천문학 전문가입니다.") ,
+      ("user", "{input}") ]
+)
+
+llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
+
+
+output_parser = StrOutputParser()
+
+# LCEL
+chain = prompt | llm | output_parser
+
+# user_input = input("질문을 입력하세요: ")
+
+# response = chain.invoke({"input": user_input})
+
+# print("Bot: ", response)
+
+import gradio as gr
+
+def chat(user_input):
+    return chain.invoke({"input": user_input})
+
+demo = gr.Interface(fn=chat, inputs="text", outputs="text")
+demo.launch()   
